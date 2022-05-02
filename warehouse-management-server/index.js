a6cc42489793620905b33fbfe4403a97e7d7964e
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-05-03 00:44:57 +0600

    new api for id addded

--- warehouse-management-server/index.js
+++ warehouse-management-server/index.js
@@ -8,10 +8,46 @@ app.use(express.json());
 require('dotenv').config();
 
 
-app.get('/', (req, res)=>{
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7s7c1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('warehouseMangement').collection('service');
+
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        app.get('/items/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const items = await productCollection.findOne(query);
+            res.send(items);
+        });
+
+
+
+    }
+    finally { }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('working');
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
+
+
+// warehouseManagement
+// zAK98yOMwQ38XoGC
